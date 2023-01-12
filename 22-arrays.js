aa8b4c1d78e5a29ae68321b3444c454826da216a
Prosen Biswas <webprosen@gmail.com>
2023-01-12 12:12:41 +0600

    staging

--- 22-arrays.js
+++ 22-arrays.js
@@ -48,6 +48,8 @@
         { title: "Pride and Prejudice", author: "Jane Austen", publisher: "Penguin" },
     ];
     
+    // console.log(books instanceof Object);
+
     function displayBookInfo(book) {
         // console.log(`Title: ${book.title}\nAuthor: ${book.author}\nPublisher: ${book.publisher}\n`);
     }
@@ -59,7 +61,7 @@
     const fruits = ["mango", "banana", "apple", "lichi", "coconut"];
     const totalItem = fruits.length; //Here value is 5
     const lastItem = fruits[fruits.length - 1]; 
-    console.log(lastItem);
+    // console.log(lastItem);
 }
 
 {
@@ -72,19 +74,45 @@
     }
 
     text += "</ul>";
-    document.getElementById("message").innerHTML = text;
+    // document.getElementById("message").innerHTML = text;
 }
 
 {
     const coder71 = ["prosen", "rajib", "anup", "jalil", "saima"];
 
     function myFunction(value) {
-        text += "<li>" + value + "</li>"
+        text += "<li>" + value + "</li>";
     }
 
     let text = "<ul>";
     coder71.forEach(myFunction);
     text += "</ul>";
 
-    document.getElementById("message").innerHTML = text;
+    // document.getElementById("message").innerHTML = text;
+}
+
+{
+    const letter = ["a", "b", "c"];
+    letter[3] = "Prosen";
+    letter.push("rajib");
+    letter[letter.length] = "Mehedi";
+    letter[10] = "Haha";
+}
+
+{
+    const myEducation = [];
+    myEducation["primary"] = "Jessore Institute Govt. Primary School Jessore";
+    myEducation["high"] = "Sammiloni Institution Jessore";
+    // console.log(myEducation);
+}
+
+{
+    // Array ort Obeject check
+    const number = [1,2,3];
+    const person = {name: "prosen"};
+
+
+    
+    // console.log(person instanceof  Array);
+    // console.log(Array.isArray(person));
 }
