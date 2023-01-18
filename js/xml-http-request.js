b0b1c276f000cafa1799a0756f5be81ce3fc0f2c
Prosen Biswas <webprosen@gmail.com>
2023-01-18 11:51:25 +0600

    fixing

--- js/xml-http-request.js
+++ js/xml-http-request.js
@@ -3,20 +3,22 @@
 // AJAX = Asynchronous JavaScript And XML
 // XML = Extensible Markup Language
 // XHR = Xml Http Request
+// GET, POST, PUT, DELETE
 
 {
     const getButton = document.getElementById('get');
     const sendButton = document.getElementById('send');
 
     const getData = function() {
+        
         const xhr = new XMLHttpRequest();
-        // GET, POST, PUT, DELETE
-        xhr.open("get", "https://ipinfo.io/json");
+        
+        xhr.open("get", "https://jsonplaceholder.typicode.com/posts/1");
         xhr.send();
         xhr.onload = function () {
             var result = xhr.response;
             result = JSON.parse(result);
-            document.getElementById('demo').innerHTML = result.city;
+            console.log(result);
         }
 
     }
@@ -29,14 +31,3 @@
     sendButton.addEventListener("click", sendData);
 
 }
-
-const fetchData = () => {
-    fetch('https://ipinfo.io/json')
-    .then((res) => res.json())
-    .then((result) => {
-        console.log(result);
-    })
-}
-
-fetchData();
-
